0cc6eba049b502e2272c8fdc2b12e812cabe8472
klein.zhou <klein.zhou@T04839-48.wh.augmentum.com>
2021-11-29 10:49:20 +0800

    threejs: add baidu tongji

---
+++ threejs/libs/tools/baidu_tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c0a9c44c77712143ff6e963faa52011a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
